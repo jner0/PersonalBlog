7c91dc7b0cd91fb8d0edae711d664044df7d5d33
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-10-30 00:20:15 -0500

    Adding styles

--- src/blog/pages/CreatePost.jsx
+++ src/blog/pages/CreatePost.jsx
@@ -1,40 +1,80 @@
-import { Box, Button, Grid, IconButton } from "@mui/material";
-import { ImageBanner } from "../components";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  TextField,
+} from "@mui/material";
+import { ImageBanner, NavBar } from "../components";
 import { SaveAltOutlined, UploadOutlined } from "@mui/icons-material";
 
 export const CreatePost = () => {
   return (
-    <Grid container>
-      <Grid container sx={{ minHeight: "250px", backgroundColor: "#e7e7e7" }}>
-        <ImageBanner images={""} />
-      </Grid>
-      <Grid item>
-        <input
-          type="file"
-          multiple
-          //   ref={fileInputRef}
-          //   onChange={onFileInputChange}
-          style={{ display: "none" }}
-        />
+    <>
+      <NavBar />
+      <Container fixed>
+        <Grid justifyContent="flex-end" container>
+          <Grid
+            container
+            sx={{ minHeight: "250px", backgroundColor: "#e7e7e7" }}
+          >
+            <ImageBanner images={""} />
+          </Grid>
+          <Grid item>
+            <input
+              type="file"
+              multiple
+              //   ref={fileInputRef}
+              //   onChange={onFileInputChange}
+              style={{ display: "none" }}
+            />
 
-        <IconButton
-          color="primary"
-          //   disabled={isSaving}
-          //   onClick={() => fileInputRef.current.click()}
-        >
-          <UploadOutlined />
-        </IconButton>
+            <IconButton
+              color="primary"
+              //   disabled={isSaving}
+              //   onClick={() => fileInputRef.current.click()}
+            >
+              <UploadOutlined />
+            </IconButton>
 
-        <Button
-          color="primary"
-          sx={{ padding: 2 }}
-          //   onClick={onSaveNote}
-          //   disabled={isSaving}
-        >
-          <SaveAltOutlined sx={{ fontSize: 30, mr: 1 }} />
-          Guardar
-        </Button>
-      </Grid>
-    </Grid>
+            <Button
+              color="primary"
+              sx={{ padding: 2 }}
+              //   onClick={onSaveNote}
+              //   disabled={isSaving}
+            >
+              <SaveAltOutlined sx={{ fontSize: 30, mr: 1 }} />
+              Guardar
+            </Button>
+          </Grid>
+          <Grid container>
+            <TextField
+              type="text"
+              variant="filled"
+              fullWidth
+              placeholder="Ingrese un Título"
+              label="Titulo"
+              sx={{ border: "none", mb: 1 }}
+              name="title"
+              //   value={title}
+              //   onChange={onInputChange}
+            />
+
+            <TextField
+              type="text"
+              variant="filled"
+              fullWidth
+              multiline
+              placeholder="Empiece a escribir aquí..."
+              minRows={20}
+              name="body"
+              //   value={body}
+              //   onChange={onInputChange}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 };
